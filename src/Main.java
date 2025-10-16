4cb8ca838d8ca9026c50780ed8e6d5cea26ac9ff
Lilian <210286008+liliann19@users.noreply.github.com>
2025-10-16 15:00:40 -0700

    Solved problem 5

--- src/Main.java
+++ src/Main.java
@@ -54,7 +54,7 @@ public class Main {
   }
 
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // O(nums)
   public static void describeProduct(int[] nums) {
     System.out.println("About to compute the product of the array...");
     int product = computeProduct(nums);
