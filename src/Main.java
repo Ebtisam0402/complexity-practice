50b5c9730bc24b233f5b14efefb7d8dc7dcb51d6
Lilian <210286008+liliann19@users.noreply.github.com>
2025-10-18 06:26:41 -0700

    Problem 7 solved

--- src/Main.java
+++ src/Main.java
@@ -73,7 +73,7 @@ public class Main {
   }
 
   // Assume that the largest number is no bigger than the length
-  // of the array
+  // of the array O(nums)
   public static void computeAllFactorials(int[] nums) {
     for(int num : nums) {
         int result = computeFactorial(num);
